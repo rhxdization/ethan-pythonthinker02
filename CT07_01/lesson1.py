09dbe496305451f6e1b17202211e05be16682843
rhx <wongethan120@gmail.com>
2025-01-09 11:04:25 +0000

    Co-authored-by: Cheryse Wee <chxrysx@users.noreply.github.com>

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,40 @@
-print("Hello from lesson 1")
+# red = 1
+# blue = 2
+# green = 3
+# ans = red*3 + blue*5 + green*4
+# print(ans)
 
+# Sample code:
+# px_red = 1
+# px_blue = 2
+# px_green = 3
+
+# num_red = 3
+# num_blue = 5
+# num_green = 4
+
+# total = (px_red * num_red) + (px_blue * num_blue) + (px_green * num_green)
+
+# print(total)
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+score = int(input("score please "))
+if score >= 75:
+    print("you got a")
+elif score >= 60:
+    print("you got b")
+elif score >= 50:
+    print("you got c")
+else:
+    print("fail")
