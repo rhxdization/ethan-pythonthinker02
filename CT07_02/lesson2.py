1a9846693b85961cc1606d778fda9ce9cf009faf
rhx <wongethan120@gmail.com>
2025-01-16 10:43:39 +0000

    Jan 16, 2025, 10:43 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -104,4 +104,5 @@ ans3 = "school"
 
 while True:
     print("Welcome to the Game Show! You will answer 3 questions and answer all 3, you get idk actually")
-    ansans1 = input("Lets start! What happened to the ")
+    ansans1 = input("Lets start! What happened to the Tiananmen Square in 1989?")
+    
