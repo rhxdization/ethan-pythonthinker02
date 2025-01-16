b7a191b65992d7c41f0bed37e3a32f4a3a06abff
rhx <wongethan120@gmail.com>
2025-01-16 10:47:08 +0000

    Jan 16, 2025, 10:47 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -108,4 +108,4 @@ while True:
         print("Wrong! Try again.")
     else:
         print("Correct! Nothing happened to the Tiananmen Square in 1989. Next question!")
-    ansans2 = int(input())
+    ansans2 = int(input("( ( ( (16)^(1/3) - log2(64) + 8 * (2^4 / 2^2) )^(1/2) + (asin(0.5) / pi) ) / ( ( (3 + (7/2))^2 / (log(1000) - log(100)) )^(1/2) + (16)^(1/4) ) ) * 2"))
