458825359f28d90f6cad73b16d1a5f80144ed44d
rhx <wongethan120@gmail.com>
2025-01-16 10:54:34 +0000

    Jan 16, 2025, 10:54 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -99,7 +99,7 @@
 # 2. Add an ability for users to skip by saying “skip”
 # 3. Disqualify user when they have tried too many times
 ans1 = "nothing"
-ans2 = "2"
+ans2 = 2
 ans3 = "school"
 counter = 0
 print("Welcome to the Game Show! You will answer 3 questions and answer all 3 correctly, you get a million vietnamese dong")
