2715a75ebc7ebf66d8375b6200795e43983fe4ea
rhx <wongethan120@gmail.com>
2025-01-16 09:35:36 +0000

    Jan 16, 2025, 9:35 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -38,13 +38,3 @@
 #     print("you got c")
 # else:
 #     print("fail")
-
-# recap 1
-
-# **Task: write a separate 'for' loop to print the following
-# numbers:**
-# 1. from 0 - 20
-# 2. from 1 to 30
-# 3. from 2 to 24 (in even numbers)
-
-for i in range()
--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,10 @@
-print("Hello from lesson 2")
 
+# recap 1
+
+# **Task: write a separate 'for' loop to print the following
+# numbers:**
+# 1. from 0 - 20
+# 2. from 1 to 30
+# 3. from 2 to 24 (in even numbers)
+
+for i in range()
