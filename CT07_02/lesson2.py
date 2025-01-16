720665da72dfa59ae537d310f56acc79ff75841c
rhx <wongethan120@gmail.com>
2025-01-16 10:33:58 +0000

    Jan 16, 2025, 10:33 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -77,7 +77,7 @@
 topping = ""
 toppinglist = ""
 while True:
-    if topping == "end" or "End":
+    if topping == "end":
         print("Your toppings are: " + toppinglist)
         break
     else:
