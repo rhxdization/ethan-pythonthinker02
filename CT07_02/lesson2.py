90f3510f15fed2817e5d4a4a3433b56463ceb003
rhx <wongethan120@gmail.com>
2025-01-16 10:48:27 +0000

    Jan 16, 2025, 10:48 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -104,7 +104,8 @@ ans3 = "school"
 counter = 0
 print("Welcome to the Game Show! You will answer 3 questions and answer all 3, you get idk actually")
 while True:
-    
+    if counter == 3:
+        
     ansans1 = input("Lets start! What happened to the Tiananmen Square in 1989?")
     if ansans1 != ans1:
         print("Wrong! Try again.")
