5c60d760cc838a8dd3ff8b3cd146fb21d6ab65be
rhx <wongethan120@gmail.com>
2025-01-16 09:50:06 +0000

    Jan 16, 2025, 9:50 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -10,7 +10,7 @@
 for i in range(2,25,2):
     print(i)
 
-## Task 1: Introduction to while loop
+# # Task 1: Introduction to while loop
 # **Task: Using a separate 'while' loop, print each of the
 # following:**
 # 1. from 0 to 20
