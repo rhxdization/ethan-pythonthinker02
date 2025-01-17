7c961c09b7bcb6e5290f966242bd9ac492086d83
rhx <wongethan120@gmail.com>
2025-01-17 15:27:12 +0000

    Jan 17, 2025, 3:27 PM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -160,11 +160,11 @@ while True:
             print("You've used up all your 3 attempts! You're disqualified!")
             break
         ansans2 = int(input("What is ( ( ( (16)^(1/3) - log2(64) + 8 * (2^4 / 2^2) )^(1/2) + (asin(0.5) / pi) ) / ( ( (3 + (7/2))^2 / (log(1000) - log(100)) )^(1/2) + (16)^(1/4) ) ) * 2? "))
-        if counter == 3:
-            break   
-        else:
-            print("Correct! The answer is 2. Final question!")
-        counter = 0
+    if counter == 3:
+        break   
+    else:
+        print("Correct! The answer is 2. Final question!")
+    counter = 0
     ansans3 = input("It is a house. Someone goes in blind, but comes out seeing. What is the house? ")
     while ansans3 != ans3:
         print("Wrong! Try again.")
