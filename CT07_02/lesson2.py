dcf6da87a928edaf16b1f9309e7b18546644ff41
rhx <wongethan120@gmail.com>
2025-01-17 15:43:08 +0000

    Jan 17, 2025, 3:43 PM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -127,10 +127,11 @@ while True:
     counter = 0
     scoreearned=5
     ansans2 = int(input("What is ( ( ( (16)^(1/3) - log2(64) + 8 * (2^4 / 2^2) )^(1/2) + (asin(0.5) / pi) ) / ( ( (3 + (7/2))^2 / (log(1000) - log(100)) )^(1/2) + (16)^(1/4) ) ) * 2? "))
-    while ansans2 != ans2:
-        print("Wrong! Try again.")
-        counter += 1
-        scoreearned-=2
+    if score != "skip":
+        while ansans2 != ans2:
+            print("Wrong! Try again.")
+            counter += 1
+            scoreearned-=2
         if counter == 3:
             print("You've used up all your 3 attempts! You're disqualified!")
             break
