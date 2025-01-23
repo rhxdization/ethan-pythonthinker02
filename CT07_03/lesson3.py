a608ab51c6fc4887b96de04591509be709eb2e2e
rhx <wongethan120@gmail.com>
2025-01-23 10:14:44 +0000

    Jan 23, 2025, 10:14 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -5,12 +5,12 @@
 #    check the answer
 # 3. While answer is not correct, repeat the question
 
-ans = "2"
+ans = 2
 riddle = "( ( ( (16)^(1/3) - log2(64) + 8 * (2^4 / 2^2) )^(1/2) + (asin(0.5) / pi) ) / ( ( (3 + (7/2))^2 / (log(1000) - log(100)) )^(1/2) + (16)^(1/4) ) ) * 2 is what "
 
 userinput = input(riddle)
 while userinput != ans:
     print("no.")
-    userinput = input(riddle)
+    userinput = int(input(riddle))
 
 print("ok you pass")
