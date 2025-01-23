25a55a0475cbde324df896e460ec171d86955d1e
rhx <wongethan120@gmail.com>
2025-01-23 10:53:23 +0000

    Jan 23, 2025, 10:53 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -77,11 +77,10 @@
 import random
 
 lives = 3
-questions = 15
 questionno = 1
 ans = 0
 no1 = random.randint(2,20)
 no2 = random.randint(2,20)
 print("hi ms tan told me to give tihs to you so uhh suffer")
-while questions != 15:
-    ans = int(input("question " + str(questionno) + ": what is " + str(no1) + " + " + str(no2) + "? "))
+while questionno != 15:
+    ans = int(input("question " + str(questionno) + ": what is " + str(no1) + " * " + str(no2) + "? "))
