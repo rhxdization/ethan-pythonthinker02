e590a94728c042739e8ad897d3368fcd03b991ed
rhx <wongethan120@gmail.com>
2025-01-23 10:09:50 +0000

    Jan 23, 2025, 10:09 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,2 +1,6 @@
-print("Hello from lesson 3")
-
+# ## Recap 1: Riddler
+# **Task: Using a while loop, code a riddler program**
+# 1. Think of a riddle
+# 2. Using the while loop, ask the user the riddle and
+#    check the answer
+# 3. While answer is not correct, repeat the question
