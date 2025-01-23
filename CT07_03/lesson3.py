867a79ed91c9b9acd9374be5e3afe02a25654888
rhx <wongethan120@gmail.com>
2025-01-23 10:40:54 +0000

    Jan 23, 2025, 10:40 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -51,4 +51,4 @@
 savings = 0
 while savings < 100:
     savings += int(input("How much did you save today? "))
-print("You saved more than $100. (" + str(savings) )
+print("You saved more than $100. (" + str(savings) + "). Congrats!")
