9108e3ed932deaffba8f7d890bbe18f8f7f26f40
rhx <wongethan120@gmail.com>
2025-01-23 10:13:54 +0000

    Jan 23, 2025, 10:13 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -8,3 +8,5 @@
 ans = "2"
 riddle = "( ( ( (16)^(1/3) - log2(64) + 8 * (2^4 / 2^2) )^(1/2) + (asin(0.5) / pi) ) / ( ( (3 + (7/2))^2 / (log(1000) - log(100)) )^(1/2) + (16)^(1/4) ) ) * 2 is what"
 
+userinput = input(riddle)
+while userinput != 
