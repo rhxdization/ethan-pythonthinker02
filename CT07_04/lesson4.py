609d1a5fb01b7fe00c22c5f10e15a365f5dc288d
rhx <wongethan120@gmail.com>
2025-02-06 09:37:48 +0000

    Feb 6, 2025, 9:37 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,2 +1,9 @@
-print("Hello from lesson 4")
+# ## Recap 1: While Loop
+# Task: Code a New Year Countdown timer. 
+# Example: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, HAPPY NEW YEAR!
+
+# 1. The countdown timer will count one by one and stop at 1.
+# 2. After 1, the program will say Happy New Year
+# 3. Use the time library to pause 1 seconds between each
+#    number
 
