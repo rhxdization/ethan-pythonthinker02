b16f36e3b0918fdbec0c00bda08749464e88c4a4
rhx <wongethan120@gmail.com>
2025-02-06 10:45:49 +0000

    Feb 6, 2025, 10:45 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -75,4 +75,5 @@ while counter != len(planets):
     print(planets[counter])
     if counter == planets.index("earth"):
         print("i live here")
-    
+    elif counter == planets.index("∫ from 0 to ∞ of (e^(-x²)) / √(x² + 1) dx"):
+        print("i conquered this")
