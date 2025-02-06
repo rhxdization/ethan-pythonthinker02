0a296182e26e966ef36ec4f83e82cc315b4d4a8b
rhx <wongethan120@gmail.com>
2025-02-06 10:26:15 +0000

    Feb 6, 2025, 10:26 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -65,7 +65,7 @@ while counter != len(planets):
 print("pluto is a planet now muahahaha")
 planets.insert(10,"pluto")
 print("ok here are the planets " + str(planets))
-
-planets.insert(3, "∫ from 0 to ∞ of (e^(-x²)) / √(x² + 1) dx")
+earth = planet.index("earth")
+planets.insert(earth, "∫ from 0 to ∞ of (e^(-x²)) / √(x² + 1) dx")
 print("i created planet ∫ from 0 to ∞ of (e^(-x²)) / √(x² + 1) dx, heres the planets " + str(planets))
 
