738a7c4ef49dbe819a048e08f6f1884807aa903b
rhx <wongethan120@gmail.com>
2025-02-13 10:14:34 +0000

    Feb 13, 2025, 10:14 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -80,4 +80,4 @@ for i in range(0,100):
     added = random.randint(1,1000)
     if added not in numbers:
         numbers.append(added)
-print("The winning numbers are: " + str(numbers))
+print("biggest score is ")
