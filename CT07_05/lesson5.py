413eeb767f41d919a3d95a85ec8f62fd06c03c8f
rhx <wongethan120@gmail.com>
2025-02-13 10:14:19 +0000

    Feb 13, 2025, 10:14 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -73,3 +73,11 @@
 # 2. Using the 'min()' function, find the minimum score.
 # 3. Using the 'sum()' and 'len()' function, calculate the
 #    average score.
+
+import random
+numbers = []
+for i in range(0,100):
+    added = random.randint(1,1000)
+    if added not in numbers:
+        numbers.append(added)
+print("The winning numbers are: " + str(numbers))
