05534e63d5f04875ac98a8247f829404a5eea846
rhx <wongethan120@gmail.com>
2025-02-13 09:41:12 +0000

    Feb 13, 2025, 9:41 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,2 +1,15 @@
-print("Hello from lesson 5")
+# # Lesson 5 - List Variables II
 
+# ## Recap 1: Favourite Food List
+# **Recap 1a**:
+# Create a list of 5 food that you like to eat
+
+# **Recap 1b**:
+# You no longer like to eat the 3rd item on your list,
+# delete it
+
+# **Recap 1c**:
+# Add 1 more item into your list
+
+# **Recap 1d**:
+# Write a for loop to say all the food items in your list
