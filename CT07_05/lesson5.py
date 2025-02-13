1031fbd26e2445d45482aed6d5f65637e3a79f2c
rhx <wongethan120@gmail.com>
2025-02-13 10:58:16 +0000

    Feb 13, 2025, 10:58 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -151,7 +151,7 @@ p2_name = input("Please enter player 2's name:")
 
 print("Welcome to the Squid Games- I meant Pokemon Games!")
 time.sleep(3)
-print("Today we have 2 contestants, *insert player 1 name* and *insert player 2 name*!")
+print("Today we have 2 contestants, " + p1_name)
 time.sleep(3)
 print("Player 1's pokemon is " + p1_pokemon + " with a power of " + str(p1_power) + "!")
 print("Player 2's pokemon is " + p2_pokemon + " with a power of " + str(p2_power) + "!")
