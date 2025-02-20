dd0b2018c2c6e8006452a1e3d49ba6650905fe83
rhx <wongethan120@gmail.com>
2025-02-20 09:40:03 +0000

    Feb 20, 2025, 9:40 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -6,20 +6,20 @@
 #         numbers.append(added)
 # print("The winning numbers are: " + str(numbers))
 
-import random
-numbers = []
-for i in range(0,100):
-    added = random.randint(1,1000)
-    if added not in numbers:
-        numbers.append(added)
-for i in numbers:
-    print(i)
-print("average score is " + str(sum(numbers) / len(numbers)))
-print("biggest score is " + str(max(numbers)))
-print("smallest score is " + str(min(numbers)))
-num = random.choice(numbers)
-num2 = numbers.index(num)
-print(numbers[num2])
+# import random
+# numbers = []
+# for i in range(0,100):
+#     added = random.randint(1,1000)
+#     if added not in numbers:
+#         numbers.append(added)
+# for i in numbers:
+#     print(i)
+# print("average score is " + str(sum(numbers) / len(numbers)))
+# print("biggest score is " + str(max(numbers)))
+# print("smallest score is " + str(min(numbers)))
+# num = random.choice(numbers)
+# num2 = numbers.index(num)
+# print(numbers[num2])
 
 
 # # Lesson 8 - String splitting, list joining, and
