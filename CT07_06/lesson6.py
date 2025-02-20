6b2c0d1ce6f886f823d5438bfe25b939642e83a7
rhx <wongethan120@gmail.com>
2025-02-20 09:32:33 +0000

    Feb 20, 2025, 9:32 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,10 +1,22 @@
+# import random
+# numbers = []
+# for i in range(0,100):
+#     added = random.randint(1,1000)
+#     if added not in numbers:
+#         numbers.append(added)
+# print("The winning numbers are: " + str(numbers))
+
 import random
 numbers = []
 for i in range(0,100):
     added = random.randint(1,1000)
     if added not in numbers:
         numbers.append(added)
-print("The winning numbers are: " + str(numbers))
+for i in numbers:
+    print(i)
+print("average score is " + str(sum(numbers) / len(numbers)))
+print("biggest score is " + str(max(numbers)))
+print("smallest score is " + str(min(numbers)))
 
 
 # # Lesson 8 - String splitting, list joining, and
