ccd7eea9c3ddb92170858652cd29b0191cba5c03
rhx <wongethan120@gmail.com>
2025-02-20 09:51:28 +0000

    Feb 20, 2025, 9:51 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -30,15 +30,15 @@
 # 2. Write a nested loop to loop through each contact and print
 #    the details for each contact
 
-contacts = []
-contact1 = ["John", 98453126, "john@gmail.com"]
-contact2 = ["Adam", 93029102, "adam@gmail.com"]
-contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
 
-contacts.append(contact1)
-contacts.append(contact2)
-contacts.append(contact3)
-for _ in contacts:
-    for i in _:
-        print(i)
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
+# for _ in contacts:
+#     for i in _:
+#         print(i)
     
