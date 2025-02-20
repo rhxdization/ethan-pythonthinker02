cc620af53210e4658c00339e0a8fa101f56e5f1f
rhx <wongethan120@gmail.com>
2025-02-20 10:57:40 +0000

    Feb 20, 2025, 10:57 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -156,7 +156,7 @@ groups = []
 students = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 size = 3
 for i in range(0, len(students), size):
-    groups = groups.append(students[i:i+size])
+    groups.append(students[i:i+size])
     print(groups)
 
 
