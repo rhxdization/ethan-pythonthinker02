71ee148c58a90f25ad2cbfd6371120c596300689
rhx <wongethan120@gmail.com>
2025-02-20 09:40:46 +0000

    Feb 20, 2025, 9:40 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -21,3 +21,18 @@
 # num2 = numbers.index(num)
 # print(numbers[num2])
 
+# ## Task 1: A list within a list 
+
+# You are about to graduate and would like to create a database
+# to keep all your friend's contact details using a 2d list.
+
+# Sample Code (Copy + Paste the below code):
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+
+# 1. append contact1, contact2, and contact3 into contacts
+# 2. Write a nested loop to loop through each contact and print
+#    the details for each contact
+
