b9f5c49b74573c42bda4038503088107d42b0daa
rhx <wongethan120@gmail.com>
2025-02-20 10:08:44 +0000

    Feb 20, 2025, 10:08 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -38,9 +38,10 @@
 # You are given 2 lists that contain the price of fruits. Now,
 # merge 2 given lists and ensure the resulting list is sorted.
 
-list1 = [3.20, 2.65, 1.75]
-list2 = [6.15, 5.45, 4.20]
-
 # 1. Merge the lists using the + operator.
 # 2. Use the sorted() function to sort the combined list.
 # 3. Print the sorted list.
+
+list1 = [3.20, 2.65, 1.75]
+list2 = [6.15, 5.45, 4.20]
+sort1 = sorted()
