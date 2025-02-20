a454d0d8d89c1ddb371e1fe815ae39a70fe49f43
rhx <wongethan120@gmail.com>
2025-02-20 10:03:42 +0000

    Feb 20, 2025, 10:03 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -15,3 +15,15 @@
 #    Name: James
 #    Phone number: 85726845
 #    CCA: Hockey
+
+# ## Task 1: Introduction to List Merging
+# You are given 2 lists of fruits. Merge them into 1 list and
+# print the result:
+
+# list1 = ["Apple", "Banana", "Cherry"]
+# list2 = ["Durian", "Elderberry", "Figs"]
+
+# 1. Use the + operator to combine the lists.
+# 2. Print the combined list.
+
+
