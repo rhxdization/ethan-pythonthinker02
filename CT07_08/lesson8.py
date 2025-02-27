a9ed0d027bafe5f5897309b5aca32973d5f8a975
rhx <wongethan120@gmail.com>
2025-02-27 09:20:19 +0000

    Feb 27, 2025, 9:20 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,1 +1,17 @@
-print("Hello from lesson 8")
+# # Lesson 8 - String splitting, list joining, and
+# #            finding substring
+
+# ## Recap 1: List Manipulation
+# Given 3 lists, merge them into a single list, remove
+# duplicates, and then split the list into 2 halves,
+# ensuring both halves are sorted.
+
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
+
+# 1. Merge the 3 lists and remove duplicates.
+# 2. Sort the resulting list.
+# 3. Split the list into 2 sorted halves.
+# 4. Print the halves.
+# 5. 
