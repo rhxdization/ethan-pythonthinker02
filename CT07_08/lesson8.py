355758e1060319a6b18ac383286d29be97bd73cd
rhx <wongethan120@gmail.com>
2025-02-27 10:09:12 +0000

    Feb 27, 2025, 10:09 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -50,5 +50,5 @@ mid = len(unique) // 2
 # slice the list for first half list1 = unique[:midpoint]
 # slice the list for second half
 for i in range(0, len(unique), mid):
-        group.append(merge[i:i+2])
+        group.append(merge[i:i+mid])
 print(group)
