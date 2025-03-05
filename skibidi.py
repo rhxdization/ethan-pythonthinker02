356f9c6143a5f92c0d2d0e9c24b515c84c72b409
rhx <wongethan120@gmail.com>
2025-03-05 08:06:55 +0000

    Mar 5, 2025, 8:06 AM

--- skibidi.py
+++ skibidi.py
@@ -2,4 +2,6 @@
 health = 100
 # Show health
 print("player has health of " + str(health))
-# While health is more than 0 start fighting
+# While health is more than 0 keep fighting
+while health > 0:
+    healthlost = random.randint(1,15)
