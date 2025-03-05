57707f1c386d112b9f473744030f19d50b570406
rhx <wongethan120@gmail.com>
2025-03-05 08:10:14 +0000

    Mar 5, 2025, 8:10 AM

--- skibidi.py
+++ skibidi.py
@@ -13,7 +13,9 @@ while health > 0:
     # Set health
     health -= healthlost
     print("player has lost " + str(healthlost) + " health and now has " + str(health))
-    if health 
-    counter += 1
+    if health < 0:
+        break
+    else:
+        counter += 1
 # Print out how many times player fought
 print("player fought " + str(counter) + " times")
