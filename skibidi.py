d5ab4da6fd72761ef2dbbd155239a30b255b2198
rhx <wongethan120@gmail.com>
2025-03-05 08:09:09 +0000

    Mar 5, 2025, 8:09 AM

--- skibidi.py
+++ skibidi.py
@@ -9,8 +9,9 @@ print("player has health of " + str(health))
 # While health is more than 0 keep fighting
 while health > 0:
     # Random number from 1 to 15 to decide how much health is lost
-    print("player has lost " + str(healthlost) + " health and now has " + str(health))
     healthlost = random.randint(1,15)
     # Set health
     health -= healthlost
+    print("player has lost " + str(healthlost) + " health and now has " + str(health))
+    counter
 # Print out how many times player fought
