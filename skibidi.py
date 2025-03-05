10b910dc9e93f419ee59902f36c647d76eab35ca
rhx <wongethan120@gmail.com>
2025-03-05 08:12:17 +0000

    Mar 5, 2025, 8:12 AM

--- skibidi.py
+++ skibidi.py
@@ -1,21 +1,23 @@
-# Import
-import random
-# Variables
-health = 100
-counter = 0
-healthlost = 0
-# Show health
-print("player has health of " + str(health))
-# While health is more than 0 keep fighting
-while health > 0:
-    # Random number from 1 to 15 to decide how much health is lost
-    healthlost = random.randint(1,15)
-    # Set health
-    health -= healthlost
-    if health < 0:
-        break
-    else:
-        counter += 1
-        print("player has lost " + str(healthlost) + " health and now has " + str(health))
-# Print out how many times player fought
-print("player fought " + str(counter) + " times")
+# # Import
+# import random
+# # Variables
+# health = 100
+# counter = 0
+# healthlost = 0
+# # Show health
+# print("player has health of " + str(health))
+# # While health is more than 0 keep fighting
+# while health > 0:
+#     # Random number from 1 to 15 to decide how much health is lost
+#     healthlost = random.randint(1,15)
+#     # Set health
+#     health -= healthlost
+#     if health < 0:
+#         break
+#     else:
+#         counter += 1
+#         print("player has lost " + str(healthlost) + " health and now has " + str(health))
+# # Print out how many times player fought
+# print("player fought " + str(counter) + " times")
+
+order = []
