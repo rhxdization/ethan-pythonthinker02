75e730e7175520f180bc24f2c8331ca8225bdd32
rhx <wongethan120@gmail.com>
2025-03-05 08:07:27 +0000

    Mar 5, 2025, 8:07 AM

--- skibidi.py
+++ skibidi.py
@@ -1,3 +1,4 @@
+# Import
 import random
 # Variables
 health = 100
@@ -5,5 +6,5 @@ health = 100
 print("player has health of " + str(health))
 # While health is more than 0 keep fighting
 while health > 0:
+    # Random number from 1 to 15 to decide how much health is lost
     healthlost = random.randint(1,15)
-    
