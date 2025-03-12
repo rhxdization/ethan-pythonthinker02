5d5ce412df741b0779b27583fecb4d1199d75708
rhx <wongethan120@gmail.com>
2025-03-12 07:50:54 +0000

    Mar 12, 2025, 7:50 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -26,15 +26,16 @@
 # duplicates, and then split the list into 2 halves,
 # ensuring both halves are sorted.
 
-list1 = [3, 2, 1]
-list2 = [6, 5, 5]
-list3 = [9, 8, 7]
-
 # # 1. Merge the 3 lists and remove duplicates.
 # # 2. Sort the resulting list.
 # # 3. Split the list into 2 sorted halves.
 # # 4. Print the halves.
 # # 5. 
+
+list1 = [3, 2, 1]
+list2 = [6, 5, 5]
+list3 = [9, 8, 7]
+
 group = []
 unique = []
 merge = list1+list2+list3
