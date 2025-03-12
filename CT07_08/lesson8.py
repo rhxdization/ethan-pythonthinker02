bafbb6d58ac9f083a162af09bfc97e8082d1b7df
rhx <wongethan120@gmail.com>
2025-03-12 08:19:03 +0000

    Mar 12, 2025, 8:19 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -12,22 +12,22 @@
 # # 3. Split the list into 2 sorted halves.
 # # 4. Print the halves.
 
-list1 = [3, 2, 1]
-list2 = [6, 5, 5]
-list3 = [9, 8, 7]
-unique = []
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
+# unique = []
 
-merge = list1+list2+list3
-for thing in merge:
-    if thing not in unique:
-        unique.append(thing)
-unique = sorted(unique)
-print(unique)
+# merge = list1+list2+list3
+# for thing in merge:
+#     if thing not in unique:
+#         unique.append(thing)
+# unique = sorted(unique)
+# print(unique)
 
-mid = len(unique) // 2
-first_half = unique[:mid]
-print(first_half)
+# mid = len(unique) // 2
+# first_half = unique[:mid]
+# print(first_half)
 
-second_half = unique[mid:]
-print(second_half)
+# second_half = unique[mid:]
+# print(second_half)
 
