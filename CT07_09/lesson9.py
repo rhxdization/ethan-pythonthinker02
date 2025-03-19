1a1a4ab893f804903ba80923c04494610861de2c
rhx <wongethan120@gmail.com>
2025-03-19 08:05:53 +0000

    Mar 19, 2025, 8:05 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,11 +1,13 @@
-ans = "egg"
-userinput = input("I must be opened before being able to use. What am I? ")
-word = []
-split = userinput.split()
-for i in split:
-    i = i.lower()
-    word.append(i)
-if ans in str(word):
-    print("✅")
-else:
-    print("❌")
+# ans = "egg"
+# userinput = input("I must be opened before being able to use. What am I? ")
+# word = []
+# split = userinput.split()
+# for i in split:
+#     i = i.lower()
+#     word.append(i)
+# if ans in str(word):
+#     print("✅")
+# else:
+#     print("❌")
+
+import turtle
