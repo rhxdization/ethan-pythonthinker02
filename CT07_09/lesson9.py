6c78b5e10a31cb1f3079b83e418300eb48f7b557
rhx <wongethan120@gmail.com>
2025-03-19 07:54:23 +0000

    Mar 19, 2025, 7:54 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,6 +1,7 @@
 ans = "school"
 userinput = input("You first enter this place blind. You then come out being able to see. What is the place? ")
 split = userinput.split()
+split = lower()
 if ans in str(split):
     print("✅")
 else:
