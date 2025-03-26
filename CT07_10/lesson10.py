bb536ec80277bf6bc1a1e9b467988216539e397c
rhx <wongethan120@gmail.com>
2025-03-26 07:45:34 +0000

    Mar 26, 2025, 7:45 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -8,8 +8,7 @@ t.shape = "turtle"
 t.fillcolor = "orange"
 
 t.seth(0)
+while True:
 for i in range(4):
-    
-    t.forward(100)
-    t.right(90)
-window.mainloop()
+        t.forward(100)
+        t.right(90)
