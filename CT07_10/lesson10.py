74987366eca87bfca1e6afb00259a8680ae37bb3
rhx <wongethan120@gmail.com>
2025-03-26 07:45:02 +0000

    Mar 26, 2025, 7:45 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -6,8 +6,10 @@ window.setup(width=600, height=400)
 t = turtle.Turtle()
 t.shape = "turtle"
 t.fillcolor = "orange"
+
 t.seth(0)
 for i in range(4):
+    
     t.forward(100)
     t.right(90)
 window.mainloop()
