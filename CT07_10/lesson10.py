8571c386687724a9a5692a431ea9b45ab8293aa2
rhx <wongethan120@gmail.com>
2025-03-26 07:42:22 +0000

    Mar 26, 2025, 7:42 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -3,12 +3,4 @@ import turtle
 window = turtle.Screen()
 window.setup(width=600, height=400)
 
-window.mainloop()
-    1. turtle.Turtle()
-    2. .shape()
-    3. .fillcolor()
-    4. .seth()
-    5. .forward()
-    6. .backward()
-    7. .left()
- .right()
+t.Turtle()
