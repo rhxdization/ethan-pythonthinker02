2fc190c9bfd44bb2a0824e02b1d93e3312a2ba71
rhx <wongethan120@gmail.com>
2025-04-02 08:20:19 +0000

    Apr 2, 2025, 8:20 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -46,6 +46,7 @@ for row in board:
     for col in row:
         if count % 3 == 0:
             print(f"| {count} |", end="\n")
+            print(_)
         else:
             print(f"| {count} ", end="")
         count += 1
