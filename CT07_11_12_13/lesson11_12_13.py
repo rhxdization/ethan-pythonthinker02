efb989509f319df232f69a3d96569ea7ebff4dd9
rhx <wongethan120@gmail.com>
2025-04-02 07:44:31 +0000

    Apr 2, 2025, 7:44 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,2 +1,19 @@
-print("Hello from lesson 11_12_13")
+# ## Recap 1: diceGuess function
+# Create a function named 'diceGuess' with in 1 parameter,
+# 'guess' (an integer between 1 and 6). The function returns True
+# if the guess matches a randomly generated number between 1 and
+# 6, and False otherwise.
 
+# 1. Import 'random' library
+# 2. Define 'diceGuess' function with 1 parameter, 'guess'
+#         a. Using '.randint()', create and assign the
+#            'random_number' variable a random number between 1
+#            and 6
+#         b. Using 'return', return the boolean value of
+#            'guess == random_number'
+
+# Usage example:
+# if diceGuess(5):
+#     print("Correct!")
+# else:
+#     print("Incorrect.")
