50c173fc15eff9652628a65ac55fb6dc4bac42b5
rhx <wongethan120@gmail.com>
2025-04-02 08:44:45 +0000

    Apr 2, 2025, 8:44 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -61,6 +61,8 @@ def get_player_move():
     while True:
         if move > 9 or move < 1 or move is not(move.isnumeric()):
             print("your input is bad")
+        else:
+            break
         move = int(input("pick a number from 1-9: "))
 get_player_move()
 print(move)
