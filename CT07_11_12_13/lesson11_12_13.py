326052bf082a95bc16b68f4bf87bd64058f609b4
rhx <wongethan120@gmail.com>
2025-04-09 08:51:47 +0000

    Apr 9, 2025, 8:51 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,90 +1,116 @@
-# ## Recap 1: diceGuess function
-# Create a function named 'diceGuess' with in 1 parameter,
-# 'guess' (an integer between 1 and 6). The function returns True
-# if the guess matches a randomly generated number between 1 and
-# 6, and False otherwise.
+# [
+#     ['X', ' ', ' ']
+#     [' ', ' ', 'O']
+#     [' ', ' ', ' ']
+# ]
 
-# 1. Import 'random' library
-# 2. Define 'diceGuess' function with 1 parameter, 'guess'
-#         a. Using '.randint()', create and assign the
-#            'random_number' variable a random number between 1
-#            and 6
-#         b. Using 'return', return the boolean value of
-#            'guess == random_number'
+# create a data structure to hold this values
+# nested list
 
-# Usage example:
-# if diceGuess(5):
-#     print("Correct!")
-# else:
-#     print("Incorrect.")
-
-# import random
-
-# def diceguess(guess):
-#     a = random.randint(1,6)
-#     if a == guess:
-#         return True
-    
-# if diceguess(4):
-#     print("✅")
-# else:
-#     print("❌")
-
-# 
 def initboard():
     board = []
+
+    # outer loop
     for i in range(3):
-        row = []
-        for j in range(3):
-            row.append(" ")
+
+        row = [] # create empty row
+        for j in range(3): # loop through 3 times to create individual cell
+            row.append(" ") # 1 cell by 1 cell
         board.append(row)
-    return board
 
-
+    return board # return the board
 
 def printboard(argboard):
-    # global board
     count = 1
     for row in argboard:
         for col in row:
-            if count % 3 == 0:
-                print(f"| {count} |", end="\n")
-                print("-"*13)
+            if col == " ":
+                print(f"| {count} ", end="")
             else:
-                # if this cell is empty
-                if col == " ":
-                    print(f"| {count} ", end="")
-                else:
-                    print(f"| {col} ", end="")
+                print(f"| {col} ", end="")
+
+            if count % 3 ==0: # help to calculate that this is a new row
+                print("|")
+                print("-"*13)
             count += 1
 
-def get_player_move(argboard):
-    global move
-   
+#### let the player choose a move
+def get_player_move(argboard, currentplayer):
     while True:
-        move = input("pick a number from 1-9: ")
-        if move.isnumeric():
+        # asking player to enter a number 1 - 9
+        move = input(f"Player {currentplayer}, Enter a number from 1 - 9: ")
+
+        # validation first
+        if move.isdigit(): # checking for numbers...
             move = int(move)
-            move -= 1
-            row = move // 3
-            col = move % 3
-            if move > 9 or move < 0:
-                print("your input sucks ngl get better lmaoooooo")
-            else:
+            if move >= 1 and move <= 9:
+                move = move - 1 # zero based index
+                row = move // 3 # return row number
+                col = move % 3 # return the col
+                print(f"row = {row}, col = {col}")
+
+                # check if this particular cell is empty
                 if argboard[row][col] == " ":
-                    argboard[row][col] == "X"
+                    argboard[row][col] = currentplayer#"X"
                     break
                 else:
-                    print(f"nevemind {move} is already taken your input sucks still")
-        else:
-            print("your input sucks ngl get better lmaoooooo")
-        # print(f"row = {row}, col = {col}")
-        # print("finally bro actually put an actual number 😭🙏")
-    return argboard
+                    print(f"{move+1} is already taken. Choose another")
 
+                # pass # i will write some code later
+            else:
+                print("Eh, must be between 1 to 9 leh.")
+        else:
+            print("Eh, you must enter number lah.")
+    return argboard # the move has been made, return the board
+
+# another function for get current player
+def getcurrentplayer(player):
+    if player == "X":
+        return "Q" 
+    else:
+        return "X"
+
+#### a function to check that you have won
+def checkwin(argboard):
+    win = [
+        # row 1 to row 3
+        [ argboard[0][0], argboard[0][1], argboard[0][2] ], # row 1
+        [ argboard[1][0], argboard[1][1], argboard[1][2] ], # row 2
+        [ argboard[2][0], argboard[2][1], argboard[2][2] ], # row 3
+
+        # col 1 to col 3
+        [ argboard[0][0], argboard[1][0], argboard[2][0] ], # col 1
+        [ argboard[0][1], argboard[1][1], argboard[2][1] ], # col 2
+        [ argboard[0][2], argboard[1][2], argboard[2][2] ], # col 3
+
+        # diagonal 1 and 2
+        [ argboard[0][0], argboard[1][1], argboard[2][2] ], # diagonal 1
+        [ argboard[0][2], argboard[1][1], argboard[2][0] ], # diagonal 2
+    ]
+
+    # loop through the win condition
+    for condition in win:
+        if condition[0] == condition[1]  == condition[2] and condition[0] != " ":
+            return True
+    return False
+
+
+# main game code
 board = initboard()
+cplayer = ""
 
 while True:
     printboard(board)
-    board = get_player_move(board)
+
+    # get current player
+    cplayer = getcurrentplayer(cplayer)
+    
+    board = get_player_move(board, cplayer)
+
+    if checkwin(board):
+        print("*"*20)
+        print(f"Player {cplayer} Wins!! ")
+        print("*"*20)
+        printboard(board)
+        break
 
