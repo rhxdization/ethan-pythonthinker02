8b7b2be32f5f785457818eda1fbae570908ce1e9
rhx <wongethan120@gmail.com>
2025-04-23 07:56:35 +0000

    Apr 23, 2025, 7:56 AM

--- CT07_11_12_13/revision.py
+++ CT07_11_12_13/revision.py
@@ -66,6 +66,9 @@ print(f"the total amount of numbers is {count}")
 # 1g. find the average of all the numbers
 
 total = 0
+for i in list1:
+    total+=i
+avg = total
 
 ######################################################
 # Question 2:
