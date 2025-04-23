642f00b40dbfc98866ef258c8f45f4c5d40bcd7b
rhx <wongethan120@gmail.com>
2025-04-23 08:17:56 +0000

    Apr 23, 2025, 8:17 AM

--- CT07_11_12_13/revision.py
+++ CT07_11_12_13/revision.py
@@ -126,6 +126,10 @@ daily_sales = [120, 98, 135, 105, 150, 112, 80, 130, 95, 110,
                132, 108, 75, 88, 142, 115, 97, 121, 89, 100]
 # Answer for Question 4 here
 
+total = 0
+for i in daily_sales:
+    total+=i
+
 
 
 
