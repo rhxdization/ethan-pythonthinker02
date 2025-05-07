5705928517b358a6e1b41ec37a86d0d7fbf1800e
rhx <wongethan120@gmail.com>
2025-05-07 07:59:15 +0000

    May 7, 2025, 7:59 AM

--- ct07sem2q1.py
+++ ct07sem2q1.py
@@ -60,45 +60,45 @@
 
 # #######################################################
 
-# You are given a list of 100 random numbers. 
-# Your task is to check if each number is odd or even.
+# # You are given a list of 100 random numbers. 
+# # Your task is to check if each number is odd or even.
 
-# To check if the number is odd or even 
-# Write a function to determine if a number is even.
-# if number is even, return True, else return False.
+# # To check if the number is odd or even 
+# # Write a function to determine if a number is even.
+# # if number is even, return True, else return False.
 
-# Example Output:
-# 2944 is even.
-# 5490 is even.
-# 2357 is odd.
-# 2619 is odd.
-
-
-##### Task 1: COMPLETE THIS FUNCTION TO CHECK ODD/ EVEN
-def is_even():
-    if i % 2 == 0:
-        return True
-    else:
-        return False
+# # Example Output:
+# # 2944 is even.
+# # 5490 is even.
+# # 2357 is odd.
+# # 2619 is odd.
 
 
-# Example output:
-list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
-         5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
-         1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884,
-         756, 6196, 5935, 5291, 8619, 2630, 1831, 3127, 4698, 6291,
-         2478, 5792, 9362, 7348, 8040, 3556, 598, 6187, 8959, 880,
-         6601, 538, 3439, 8508, 8649, 5139, 8076, 78, 6776, 362,
-         6368, 6460, 8604, 1763, 1713, 2354, 2167, 6612, 8149, 7961,
-         4270, 5285, 7346, 5667, 2102, 900, 8063, 4577, 2285, 9592,
-         5671, 537, 9777, 9421, 5455, 1241, 990, 3745, 8443, 4213,
-         4183, 2463, 9562, 8137, 5101, 397, 6966, 9927, 7473, 4105]
+# ##### Task 1: COMPLETE THIS FUNCTION TO CHECK ODD/ EVEN
+# def is_even():
+#     if i % 2 == 0:
+#         return True
+#     else:
+#         return False
 
-###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
-for i in list1:
-    if is_even():
-        print(f"{i} is even")
-    else:
-        print(f"{i} is odd")
 
-    # call the function to check if the number is odd or even
+# # Example output:
+# list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
+#          5972, 7532, 4382, 8311, 6664, 4918, 3656, 3769, 6179, 7720,
+#          1777, 7149, 2175, 8665, 4586, 5208, 320, 1314, 8950, 4884,
+#          756, 6196, 5935, 5291, 8619, 2630, 1831, 3127, 4698, 6291,
+#          2478, 5792, 9362, 7348, 8040, 3556, 598, 6187, 8959, 880,
+#          6601, 538, 3439, 8508, 8649, 5139, 8076, 78, 6776, 362,
+#          6368, 6460, 8604, 1763, 1713, 2354, 2167, 6612, 8149, 7961,
+#          4270, 5285, 7346, 5667, 2102, 900, 8063, 4577, 2285, 9592,
+#          5671, 537, 9777, 9421, 5455, 1241, 990, 3745, 8443, 4213,
+#          4183, 2463, 9562, 8137, 5101, 397, 6966, 9927, 7473, 4105]
+
+# ###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
+# for i in list1:
+#     if is_even():
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")
+
+#     # call the function to check if the number is odd or even
