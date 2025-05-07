3f4dd80896aefa02d326c743144a8661f798d5ee
rhx <wongethan120@gmail.com>
2025-05-07 07:47:26 +0000

    May 7, 2025, 7:47 AM

--- ct07sem2q1.py
+++ ct07sem2q1.py
@@ -50,7 +50,7 @@ avg = round(total / counter,2)
 # print out statistics
 print(f"{maxday} August has highest sales of ${max}")
 print(f"{minday} August has lowest sales of ${min}")
-print()
+print(f"Average sales of August is ${avg}")
 
 
 
