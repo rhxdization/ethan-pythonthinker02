10eb08b813d3f3b497a7a3815599aec489352b8a
rhx <wongethan120@gmail.com>
2025-05-07 07:57:02 +0000

    May 7, 2025, 7:57 AM

--- ct07sem2q1.py
+++ ct07sem2q1.py
@@ -97,6 +97,6 @@ list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
 ###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
 for i in list1:
     if is_even(i):
-        print("")
+        print(f"{i} is ")
 
     # call the function to check if the number is odd or even
