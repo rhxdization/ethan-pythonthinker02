a1bad21bc9cb1a252d8f20dc30f495ac90795caa
rhx <wongethan120@gmail.com>
2025-05-14 08:09:34 +0000

    May 14, 2025, 8:09 AM

--- CT07_14_15_16/whitejack.py
+++ CT07_14_15_16/whitejack.py
@@ -37,6 +37,7 @@ def show_hand(hand, typeofhand):
             print(f" {card[1]} {card[0]}")
         print("😀"*10)
     elif typeofhand == "banker_h":
+        pri
         print("😈" * 10)
         print("Banker Hand")
         print(f" {hand[0][1]} {hand[0][0]}\n {"? ? " * 3}")
