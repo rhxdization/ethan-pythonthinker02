bbb6640ecde078021e6ba96d623202d3807c0fae
rhx <wongethan120@gmail.com>
2025-05-14 08:05:31 +0000

    May 14, 2025, 8:05 AM

--- CT07_14_15_16/whitejack.py
+++ CT07_14_15_16/whitejack.py
@@ -32,7 +32,7 @@ player_hand = [deck.pop(), deck.pop()]
 def show_hand(hand, typeofhand):
     if typeofhand == "player_s":
         for card in hand:
-            print(f"{card[1]} {card[0]}")
+            print(f" {card[1]} {card[0]}")
     elif typeofhand == "banker_h":
-        print(f"{hand[0][1]} {hand[0][0]}\n {"? ? " * 3}")
+        print(f" {hand[0][1]} {hand[0][0]}\n {"? ? " * 3}")
 show_hand(player_hand, "banker_h")
